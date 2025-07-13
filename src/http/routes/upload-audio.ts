9b75f117832b78cc6aaac7a33cdf367184645939
kassianoshiguematsu <kassiano.e@hotmail.com>
2025-07-13 19:39:57 -0300

    feat: add gemini to transcription text

--- src/http/routes/upload-audio.ts
+++ src/http/routes/upload-audio.ts
@@ -2,6 +2,7 @@ import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
 import { z } from "zod/v4"
 import { db } from "../../db/connection.ts"
 import { schema } from "../../db/schema/index.ts"
+import { transcribeAudio } from "../../services/gemini.ts"
 
 export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -20,6 +21,13 @@ export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
       if (!audio) {
         throw new Error("Audio is required")
       }
+
+      const audioBuffer = await audio.toBuffer()
+      const audioAsBase64 = audioBuffer.toString("base64")
+
+      const transcription = await transcribeAudio(audioAsBase64, audio.mimetype)
+
+      return { transcription }
     }
   )
 }
