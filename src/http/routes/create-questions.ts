458ccaf0a7a312b7ba363ad70d106563770bd556
kassianoshiguematsu <kassiano.e@hotmail.com>
2025-07-13 20:55:53 -0300

    feat: add similarity

--- src/http/routes/create-questions.ts
+++ src/http/routes/create-questions.ts
@@ -1,9 +1,9 @@
+import { and, eq, sql } from "drizzle-orm"
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod"
 import { z } from "zod/v4"
 import { db } from "../../db/connection.ts"
 import { schema } from "../../db/schema/index.ts"
 import { generateAnswer, generateEmbeddings } from "../../services/gemini.ts"
-import { and, eq, sql } from "drizzle-orm"
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -23,23 +23,24 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
       const { question } = request.body
 
       const embeddings = await generateEmbeddings(question)
+
       const embeddingsAsString = `[${embeddings.join(",")}]`
 
       const chunks = await db
         .select({
           id: schema.audioChunks.id,
           transcription: schema.audioChunks.transcription,
-          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddings}::vector)`,
+          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
         })
         .from(schema.audioChunks)
         .where(
           and(
             eq(schema.audioChunks.roomId, roomId),
-            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddings}::vector)`
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
           )
         )
         .orderBy(
-          sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddings}::vector)`
+          sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
         )
         .limit(3)
 
--- src/http/routes/upload-audio.ts
+++ src/http/routes/upload-audio.ts
@@ -26,24 +26,8 @@ export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
       const audioAsBase64 = audioBuffer.toString("base64")
 
       const transcription = await transcribeAudio(audioAsBase64, audio.mimetype)
-      const embeddings = await generateEmbeddings(transcription)
 
-      const result = await db
-        .insert(schema.audioChunks)
-        .values({
-          roomId,
-          transcription,
-          embeddings,
-        })
-        .returning()
-
-      const chunk = result[0]
-
-      if (!chunk) {
-        throw new Error("Erro ao salvar chunk de áudio!")
-      }
-
-      return reply.status(201).send({ chunkId: chunk.id })
+      return { transcription }
     }
   )
 }
